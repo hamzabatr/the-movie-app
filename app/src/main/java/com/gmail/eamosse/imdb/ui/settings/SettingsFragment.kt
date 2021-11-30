b6dcc9f130998f1b8f4bfc30f56acc25160cb1ec
j.roussy <j.roussy@net.estia.fr>
2021-11-30 19:11:37 +0100

    Commit About and Settings

---
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/settings/SettingsFragment.kt
@@ -0,0 +1,49 @@
+package com.gmail.eamosse.imdb.ui.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.preference.* // ktlint-disable no-wildcard-imports
+import com.gmail.eamosse.imdb.R
+
+class SettingsFragment :
+    PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        // gérer le choix des langues
+        // gérer le mode sombre grace a un switch
+        // gérer les paramètres de notifications :
+        //      - lorsque les notifications sont désaciver les paramètre de mode et de volume le sont aussi
+        //      - lorsque le mode vibreur ou le mode silencieux est activé, le volume est a 0 est le slider ne peut pas etre modifier
+
+        settings()
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        settings()
+        return super.onPreferenceTreeClick(preference)
+    }
+
+    private fun settings() {
+        val sp = PreferenceManager.getDefaultSharedPreferences(context)
+
+        val language = sp.getString("languages", "")
+        val night_mode = sp.getBoolean("night_mode", true)
+        val notifications = sp.getBoolean("notifications", true)
+        val notifications_mode = sp.getString("notifications_mode", "")
+        val volume = sp.getInt("volume_notifications", 0)
+
+        if (night_mode == true) {
+            AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        }
+
+        findPreference<ListPreference>("notifications_mode")?.setEnabled(!notifications)
+        findPreference<SeekBarPreference>("notifications_volume")?.setEnabled(!notifications && !(notifications_mode.equals(getString(R.string.vibrate)) || notifications_mode.equals(getString(R.string.silent))))
+    }
+}
+
