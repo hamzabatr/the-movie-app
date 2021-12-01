27099bb0bb1e06d0ff872873f97e46aca076c22b
TheoSaubanere <t.saubanere@net.estia.fr>
2021-12-01 14:06:38 +0100

    Names changing

--- app/src/main/java/com/gmail/eamosse/imdb/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/discover/DiscoverFragment.kt
@@ -1,4 +1,4 @@
-package com.gmail.eamosse.imdb.ui.dashboard
+package com.gmail.eamosse.imdb.ui.discover
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.gmail.eamosse.imdb.R
 
-class DashboardFragment : Fragment() {
+class DiscoverFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var discoverViewModel: DiscoverViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(
+        discoverViewModel =
+            ViewModelProviders.of(this).get(DiscoverViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_discover, container, false)
+        val textView: TextView = root.findViewById(R.id.text_discover)
+        discoverViewModel.text.observe(
             viewLifecycleOwner,
             Observer {
                 textView.text = it
